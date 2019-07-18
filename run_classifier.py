9c737cafab8f58e68e49c56a82d5012604dad8a8
faizansuhail89 <51496123+faizansuhail89@users.noreply.github.com>
2019-07-18 12:58:08 +0300

    Add files via upload

--- run_classifier.py
+++ run_classifier.py
@@ -62,7 +62,8 @@ flags.DEFINE_bool(
     "models and False for cased models.")
 
 flags.DEFINE_integer(
-    "max_seq_length", 128,
+#   "max_seq_length", 128,
+    "max_seq_length", 3000, 
     "The maximum total input sequence length after WordPiece tokenization. "
     "Sequences longer than this will be truncated, and sequences shorter "
     "than this will be padded.")
