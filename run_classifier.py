7f9399075c1cd37effa177442dd0c7d403012e07
faizansuhail89 <51496123+faizansuhail89@users.noreply.github.com>
2019-08-30 18:13:25 +0300

    Add files via upload

--- run_classifier.py
+++ run_classifier.py
@@ -674,7 +674,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
     per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
     loss = tf.reduce_mean(per_example_loss)
-
+    tf.print("Loss: ", loss, output_stream=sys.stdout)
     return (loss, per_example_loss, logits, probabilities)
 
 
