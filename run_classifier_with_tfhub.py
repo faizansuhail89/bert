f82966191d02e8f8809a158ea8050ebaa3d21f17
faizansuhail89 <51496123+faizansuhail89@users.noreply.github.com>
2019-08-30 17:56:41 +0300

    Add files via upload

--- run_classifier_with_tfhub.py
+++ run_classifier_with_tfhub.py
@@ -80,7 +80,7 @@ def create_model(is_training, input_ids, input_mask, segment_ids, labels,
 
     per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
     loss = tf.reduce_mean(per_example_loss)
-
+    print(loss)
     return (loss, per_example_loss, logits, probabilities)
 
 
